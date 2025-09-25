b8b0834f731d6bdc8c5c958dbe56f065f47b7072
SALIOU Yves-Marie <yves-marie.saliou@dauphine.eu>
2025-09-25 18:14:11 +0200

    initialize __init__.py for linear regression module

--- src/linear_regression/__init__.py
+++ src/linear_regression/__init__.py
@@ -1,4 +1,2 @@
 from linear_regression.linear_regression import LinearRegression
-# No additional code needed here for authentication.
-# For publishing to PyPI, use an API token instead of username/password.
-# See: https://pypi.org/help/#apitoken
+
